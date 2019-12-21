feccfc324d0e273c1de4f3efa304d9ab1120fe6b
Kieran Lord <cratesmith@ratesmith.com>
2019-12-21 12:39:23 +1000

    Moved Collections into it's own package

---
+++ SubComponent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cratesmith.OnOff
+{
+    public abstract class SubComponent<T> : MonoBehaviour
+    {
+        private T m_owner;
+        public T owner 
+        {
+            get 
+            {
+                if(EqualityComparer<T>.Default.Equals(m_owner, default(T)))
+                {
+                    m_owner = GetComponentInParent<T>();
+                }
+                return m_owner;
+            }
+        }
+    }
+}
